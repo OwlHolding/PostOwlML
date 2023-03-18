f70610d25c1fa7f6184ae94a9423bcef433d7335
Kirill72-web <kirill.lego-kot@ya.ru>
2023-03-18 11:07:08 +0300

    add-test-for-fastapi

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 import core.telegram as telegram
 import asyncio
+from starlette.testclient import TestClient
+from app import app
+
+client = TestClient(app)
 
 
-def test_channels():
+def test_telegram_channels():
     assert asyncio.run(telegram.get_posts("forbesrussia", 10, 0))[1] == 200
     assert asyncio.run(telegram.get_posts("mintsifry", 10, 0))[1] == 200
     assert asyncio.run(telegram.get_posts("SKOLKOVO_Start", 10, 0))[1] == 200
 
 
-def test_count():
+def test_telegram_count():
     assert asyncio.run(telegram.get_posts("forbesrussia", 100, 0))[1] == 200
     assert asyncio.run(telegram.get_posts("forbesrussia", 500, 0))[1] == 200
 
+
+def test_ping():
+    response = client.post('/register/1')
+    assert response.status_code // 10 == 20
 
